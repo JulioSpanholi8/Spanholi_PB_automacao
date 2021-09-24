f4b63f5ffdd63e249133504b7654319070051076
Julio Cesar Spanholi <jcspanholi30@hotmail.com>
2021-09-24 16:57:25 -0300

    Adicionando correções exercicio_5

--- Atividade_24_09-js/exercicio_5.js
+++ Atividade_24_09-js/exercicio_5.js
@@ -1,12 +1,13 @@
 var x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 var media = 0
-var a = 0
-var b = 0
+var soma = 0
+var cont = 0
 
-for(var count = 1; count <= x.length; count++){
-    if(count % 2 ==0){
-     a += 1
-     b += count
+for(let i = 0; i <= x.length; i++){
+    if(x[i] % 2 == 0){
+     cont ++
+     soma += x[i]
     }
 }
-console.log(media = a/b)
+media = soma / cont
+console.log(media)
