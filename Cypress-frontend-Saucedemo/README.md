1fa1344d430f8a821beaf9d8e6eff894288fc5f8
Julio Cesar Spanholi <jcspanholi30@hotmail.com>
2021-10-29 01:05:56 -0300

    Atualizando testes - Site Saucedemo

---
+++ Cypress-frontend-Saucedemo/README.md
@@ -0,0 +1,7 @@
+<h1 align="center"> Spanholi_PB_automacao </h1>
+<h3> Júlio César Spanholi </h3>
+Repositório para a realização das atividades do Programa de Bolsas Compasso.
+
+<h3> Atividade Sprint 4 - Cypress para FrontEnd </h3>
+
+![Saucedemo_atividade](https://user-images.githubusercontent.com/73197719/139330294-37254692-dd03-4e07-ba74-d3d97349bb42.PNG)
---
+++ Cypress-frontend-Saucedemo/cypress/pages/components/menu.elements.js
@@ -0,0 +1,11 @@
+export const MENU = {
+    MENU_PG     : '#react-burger-menu-btn',
+    MENU_LIST   : '.bm-item-list',
+    MENU_ITENS  : '#inventory_sidebar_link',
+    MENU_SOBRE  : '#about_sidebar_link',
+    MENU_SAIR   : '#logout_sidebar_link',
+    MENU_RESET  : '#reset_sidebar_link', 
+    URL_ITENS   : '/inventory.html',   
+    URL_SOBRE   : '/saucelab', 
+    URL_SAIR    : 'saucedemo'
+}
